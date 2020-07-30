f425a91a91fb4222516aa634dfd004b8dc70876a
forcheng <2871979271@qq.com>
2020-07-30 17:40:01 +0800

    forcheng - update `Subject` example 5

--- src/subject/example-5.ts
+++ src/subject/example-5.ts
@@ -6,6 +6,10 @@ import { ReplaySubject } from 'rxjs';
 
 const subject = new ReplaySubject(3); // 为新的订阅者缓冲3个值
 
+// 除了缓冲数量，可以指定 window time (以毫秒为单位)来确定多久之前的值可以记录
+// 可以缓存10 个，但 window time 参数只设置了500毫秒
+// const subject = new ReplaySubject(10, 500);
+
 subject.subscribe({
     next: (v) => console.log('observerA: ' + v)
 });
